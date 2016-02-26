0a87fb3f892afcbcf2fd47a9c4084887f1f4eee4
Steven Wooding <steve@stevenwooding.com>
2016-02-26 16:58:24 +0000

    feat: One enemy going across the screen
    No player at the moment. Just tried to get
    the enemy mechanics done first, starting
    with one.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,13 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    // Set the inital position of the enemy
+    this.x = 200;  // TODO Make appear randomly along the screen
+    this.y = 231;  // TODO Make appear randomly on 1 of 3 stone block rows (65, 148 or 231)
+
+    // Set the speed of the enemy
+    this.speed = 100; // TODO Make random
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +21,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += (this.speed * dt);
+
+    // When the enemy goes off the screen, wrap it back on the other side of the screen
+    if (this.x > 505) {  // TODO Get this from the canvas
+        this.x = -101;   // TODO This is the width of the sprite. Can we get this from the sprite?
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -28,6 +41,8 @@ Enemy.prototype.render = function() {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+allEnemies.push(new Enemy());
 // Place the player object in a variable called player
 
 
@@ -42,5 +57,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    // player.handleInput(allowedKeys[e.keyCode]);
 });
